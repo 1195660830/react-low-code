11eb131b02583734df8c10a1be205f1f9ca47f6f
weikaidi <1195660830@qq.com>
2022-10-30 23:39:32 +0800

    init

--- template/src/App.js
+++ template/src/App.js
@@ -1,12 +1,37 @@
+import Header from "./layout/Header";
+import Left from "./layout/Left";
+import Center from "./layout/Center";
+import Right from "./layout/Right";
+import styles from "./App.less";
+import {useCanvas, useGetCanvas} from "./store/hooks";
+import {CanvasContext} from "./Context";
+import {useEffect, useLayoutEffect, useReducer} from "react";
 
-import style from './App.less';
+export default function App(props) {
+  const canvas = useCanvas();
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
-function App() {
+  useEffect(() => {
+    const unsubscribe = canvas.subscribe(() => {
+      forceUpdate();
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const data = useGetCanvas(canvas);
   return (
-    <div className={style.App}>
-      1231231
+    <div className={styles.main}>
+      <CanvasContext.Provider value={canvas}>
+        <Header />
+        <div className={styles.content}>
+          <Left />
+          <Center />
+          <Right />
+        </div>
+      </CanvasContext.Provider>
     </div>
   );
 }
-
-export default App;
